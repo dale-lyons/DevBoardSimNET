99ca1f1818a602bd7dfaaf4d0a99e5e8d54c44ff
Dale Lyons <wdlyons@gmail.com>
2024-11-26 15:10:30 -0800

    Add files via upload

---
+++ GUI/IView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI
+{
+    public enum ViewEnums
+    {
+        CodeT = 0,
+        RegistersT = 1,
+        OutputT = 2,
+        MemoryT = 3,
+        BoardT = 4,
+        OpCodeTraceT = 5,
+        WatchT = 6,
+        StackT = 7
+    }
+    public delegate IView QueryViewFunc(ViewEnums view);
+
+    public interface IView
+    {
+        void ResetView();
+        void RefreshView();
+        void Start();
+        void Stop();
+    }
+}
---
+++ GUI/ToolWindows.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace GUI
+{
+    public partial class ToolWindows : DockContent
+    {
+        public ToolWindows()
+        {
+            InitializeComponent();
+            AutoScaleMode = AutoScaleMode.Dpi;
+        }
+    }
+}
